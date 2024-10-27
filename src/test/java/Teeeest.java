b21b2e19c08fc94d0027b3bdbc7c60667f00d1b8
Alexandr Hapon <full8back@gmail.com>
2024-10-27 17:47:21 +0300

    12345678910

--- src/test/java/Teeeest.java
+++ src/test/java/Teeeest.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 public class Teeeest {
 
     @Test
